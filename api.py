99c7333d610fe66de2983ff1123e6bb218909f57
pradish-pranam <pradishpranam.s@mind-graph.com>
2023-11-14 17:46:41 +0530

    added cors

--- api.py
+++ api.py
@@ -14,12 +14,11 @@ app = Flask(__name__)
 
 conn = duckdb.connect(database=':memory:')
 
-# origins = [    "http://localhost.tiangolo.com",    "https://localhost.tiangolo.com",    "http://localhost",    "http://localhost:8080"],app.add_middleware(  CORSMiddleware,allow_origins=origins,    allow_credentials=True,    allow_methods=["*"],    allow_headers=["*"],)
 
-# CORS(app,origins="*")
+CORS(app,origins="*")
 
 
-CORS(app, resources={r"/*": {"origins": ["http://localhost.tiangolo.com", "https://localhost.tiangolo.com", "http://localhost", "http://localhost:8080"]}, "supports_credentials": True})
+# CORS(app, resources={r"/*": {"origins": ["http://localhost.tiangolo.com", "https://localhost.tiangolo.com", "http://localhost", "http://localhost:8080"]}, "supports_credentials": True})
 
 @app.route('/milestones', methods = ['GET'])
 def milestones():
@@ -175,5 +174,5 @@ def profile():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,port=8050)
+    app.run(host='0.0.0.0',port=80)
 
